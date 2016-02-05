d606c690511ab7256c1a8231bc907a1c672eef2a
gunnarnormand <gunnarnormand@yahoo.com>
2016-02-04 20:21:17 -0500

    main structure

--- week-four-hw/js/script.js
+++ week-four-hw/js/script.js
@@ -6,8 +6,20 @@ $(document).ready(function(){
 		$('.bar').toggleClass('clicked');
 	});
 
-	$('.burg').on("click", function (){
+	$('.burg').on('click', function (){
 
 		$('nav').toggleClass('overlay');
 	});
-});
+
+	$('.btn-big').click(function() {
+
+		$(this).html('V');
+
+		$('.expands').addClass("open");
+	});
+
+	$('.btn-small').click(function() {
+
+		$('.expands').removeClass("open");
+	});
+});	
